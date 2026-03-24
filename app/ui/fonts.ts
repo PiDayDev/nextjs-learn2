ce1467231a18542ce8b224a62f040f5df436d8ea
Damiano Salvi <salvi@intre.it>
2026-03-24 10:38:13 +0100

    Chapter 3 -Fonts, images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Iceberg, Inter, Lusitana} from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700']});
+export const iceberg = Iceberg({subsets: ['latin'], weight:['400']});
